c3ff48bf414d0bedc4be9bb0bf93237a75217c78
Manu Sunny <manupsunny@gmail.com>
2017-11-25 12:07:35 +0700

    Creating executable for Authentication.API

--- Authentication.API/Program.cs
+++ Authentication.API/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace Authentication.API
+﻿using Topshelf;
+
+namespace Authentication.API
 {
     class Program
     {
         static void Main(string[] args)
         {
+            HostFactory.Run(x =>
+            {
+                x.Service<Startup>(s =>
+                {
+                    s.ConstructUsing(name => new Startup());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetDescription("OAuth Authentication Service");
+                x.SetDisplayName("OAuth Authentication Service");
+                x.SetServiceName("OAuth-Authentication-Service");
+                x.StartAutomatically();
+            });
         }
     }
 }
---
+++ Authentication.API/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace Authentication.API
+{
+    public class Startup
+    {
+        private IDisposable WebAppInstance;
+
+        public void Start()
+        {
+            var url = "http://localhost:9001";
+            WebAppInstance = WebApp.Start(url);
+        }
+
+        public void Stop()
+        {
+            WebAppInstance?.Dispose();
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Authentication Service started..");
+            });
+        }
+    }
+}
