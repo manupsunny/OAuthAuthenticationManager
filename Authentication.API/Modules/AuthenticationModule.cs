b99f974d676be369c2c194d51e14d694f1f3dafa
Manu Sunny <manupsunny@gmail.com>
2017-11-27 10:52:30 +0700

    Adding authentication module

---
+++ Authentication.API/Modules/AuthenticationModule.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Nancy;
+
+namespace Authentication.API.Modules
+{
+    public class AuthenticationModule : NancyModule
+    {
+        public AuthenticationModule() : base("/authentication")
+        {
+            Post["/login", true] = async (x, ct) =>
+            {
+                return await Login();
+            };
+
+            Post["/logout", true] = async (x, ct) =>
+            {
+                return await Logout();
+            };
+        }
+
+        private Task<dynamic> Logout()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private Task<dynamic> Login()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
